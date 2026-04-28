6c39580d871e72aa9996ece3cdc11ac1bb9caf9a
himalaya-pahar <nafisshahriar82@gmail.com>
2026-04-28 23:26:06 +0600

    updated wishlist

--- routers/wishlist.py
+++ routers/wishlist.py
@@ -13,9 +13,9 @@ router=APIRouter(
 
 
 @router.post("/", response_model=schemas.ShowWishlist)
-def add_to_wishlist(wish_item: schemas.WishlistCreate, session: d_b.SessionDep):
+def add_to_wishlist(wish_item: schemas.WishlistCreate, session: d_b.SessionDep,current_user=Depends(oauth2.get_current_user)):
     return wishlist.add_to_wishlist(wish_item,session)
 
 @router.get("/", response_model=list[schemas.ShowWishlist])
-def get_wishlist(session: d_b.SessionDep):
+def get_wishlist(session: d_b.SessionDep,current_user=Depends(oauth2.get_current_user)):
     return wishlist.get_wishlist(session)
