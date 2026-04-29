150391c102c95259c4cc4ee4e1cdb9bd95f10905
himalaya-pahar <nafisshahriar82@gmail.com>
2026-04-29 09:19:50 +0600

    fix middleware

--- main.py
+++ main.py
@@ -14,16 +14,10 @@ async def lifespan(app:FastAPI):
 
 app=FastAPI(lifespan=lifespan)
 
-origins = [
-    "http://localhost:5500",   
-    "http://127.0.0.1:5500",   
-    "http://localhost:3000",   
-    "http://127.0.0.1:8000",   
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,      
+    allow_origins=["*"],      
     allow_credentials=True,
     allow_methods=["*"],        
     allow_headers=["*"],        
